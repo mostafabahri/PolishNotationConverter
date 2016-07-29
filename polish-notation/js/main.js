d09ad1b0908b367884e1b9c7ad01231f6288f8e7
Mostafa <potter6000@gmail.com>
2016-07-29 22:04:25 +0430

    notation partially done.error handling left. and DOM requires more attention. improvements in view.  Changes to be committed: 	modified:   css/main.css 	modified:   index.html 	modified:   js/main.js 	modified:   js/notation.js

--- polish-notation/js/main.js
+++ polish-notation/js/main.js
@@ -1,91 +1,116 @@
-// 'use strict';
-// $('#prefixInput').focus(function(event) {
-// 	// $('#postfixInput').prop('disabled',true).attr('value', " ");
-// 	// $('#infixInput').prop('disabled',true).attr('value', " ");
-// 	deactivate('infixInput');
-// 	deactivate('postfixInput');
-
-// });
-
-
-// $('#prefixInput').focusout(function(event) {
-// 	activate('postfixInput');
-// 	activate('infixInput');
-// });
-
-// console.log(inputIDs.join(","))
-// for (var i = 0; i < inputIDs.length; i++) {
-// 		$('#'+inputIDs[i]).focus(function(event) {
-// 			deactivate(inputIDs[i]);
-// 		}).focusout(function(event) {
-// 			activate(inputIDs[i])
-// 		});
-// }
+// 'use strict'
 function readonlify(arg) {
-    $(arg).prop('readonly', true).attr('value', "computing...");
+    return $(arg).prop('readonly', true).attr('value', "computing...");
 }
 
 function readwritify(arg) {
-    $(arg).prop('readonly', false).attr('value', "");
+    return $(arg).prop('readonly', false).attr('value', null);
 }
-
-var inputIDs = [];
-$(".container").find(":input").each(function() {
-    inputIDs.push(this.id);
-});
-var res = prefixToInfix("*/345");
-console.log(res);
+//
+// var inputIDs = [];
+// $(".container").find(":input").each(function() {
+//     inputIDs.push(this.id);
+// });
+// var res = prefixToInfix("*/345");
+// console.log(res);
 
 var notes = $(".container ").find(":input ");
 var _pre = notes[0],
     _in = notes[1],
     _post = notes[2];
 
-$(notes[0]).on('input', function(event) {
-    var res = prefixToInfix(notes[0].value);
-    if (res != -1) {
-        notes[1].value = res;
+notes.each(function(index) {
+    $(this).focus(function() {
+        $(this).select();
+    });
+});
+
+$(_pre).on('input', function(event) {
+    var res = prefixToInfix(this.value);
+    if (res != -1 && res.length > 1) {
+        _in.value = res;
+        _post.value = prefixToPostfix(this.value);
     }
 }).focusout(function functionName() {
-    readwritify(notes[1]);
-    readwritify(notes[2]);
-    var res = prefixToInfix(notes[0].value);
+    readwritify(_in);
+    readwritify(_post);
+    var res = prefixToInfix(this.value);
     if (res == -1) {
         $(this).parent().addClass('has-error');
     } else {
-        notes[1].value = res;
+        _in.value = res;
+        console.log(prefixToPostfix(this.value));
+        _post.value = prefixToPostfix(this.value);
         $(this).parent().removeClass('has-error');
         $(this).parent().addClass('has-success');
     }
 }).focusin(function functionName() {
     console.log("focused in ");
-    // notes[1].value=notes[2].value="";
-    readonlify(notes[1]);
-    readonlify(notes[2]);
+    readonlify(_in);
+    readonlify(_post);
+}).keyup(function() {
+    if (!this.value) {
+        _in.value = _post.value = null;
+        // $(this).parent().removeClass('has-error').removeClass('has-success');
+    }
 });
 
 
-
-//postfix
-$(notes[2]).on('input', function(event) {
-    var res = postfixToInfix(notes[2].value);
-    if (res != -1) {
-        notes[1].value = res;
+$(_in).on('input', function(event) {
+    var res = infixtoPostfix(this.value);
+    if (res != -1 && res.length > 1) {
+        _post.value = res;
+        _pre.value = infixToPrefix(this.value);
     }
 }).focusout(function functionName() {
-    readwritify(notes[0]);
-    readwritify(notes[1]);
-    var res = postfixToInfix(notes[2].value);
+    readwritify(_pre);
+    readwritify(_post);
+    var res = infixtoPostfix(this.value);
     if (res == -1) {
         $(this).parent().addClass('has-error');
     } else {
-        notes[1].value = res;
+        _post.value = res;
+        _pre.value = infixToPrefix(this.value);
         $(this).parent().removeClass('has-error');
         $(this).parent().addClass('has-success');
     }
 }).focusin(function functionName() {
     console.log("focused in ");
-    // notes[1].value=notes[2].value="";
-    readonlify(notes[0]);
-    readonlify(notes[1]);
+    readonlify(_pre);
+    readonlify(_post);
+}).keyup(function() {
+    if (!this.value) {
+        _pre.value = _post.value = null;
+        // $(this).parent().removeClass('has-error').removeClass('has-success');
+    }
+});
+
+$(_post).on('input', function(event) {
+    var res = postfixToInfix(this.value);
+    if (res != -1 && res.length > 1) {
+        _in.value = res;
+        _pre.value = postfixToPrefix(this.value);
+    }
+}).focusout(function functionName() {
+    readwritify(_in);
+    readwritify(_pre);
+    var res = postfixToInfix(this.value);
+    if (res == -1) {
+        $(this).parent().addClass('has-error');
+    } else {
+        _in.value = res;
+        // console.log(postfixToPrefix(this.value));
+        _pre.value = postfixToPrefix(this.value);
+        $(this).parent().removeClass('has-error');
+        $(this).parent().addClass('has-success');
+    }
+}).focusin(function functionName() {
+    console.log("focused in ");
+    readonlify(_in);
+    readonlify(_pre);
+}).keyup(function() {
+    if (!this.value) {
+        _in.value = _pre.value = null;
+        // $(this).parent().removeClass('has-error').removeClass('has-success');
+    }
 });
--- polish-notation/js/notation.js
+++ polish-notation/js/notation.js
@@ -4,11 +4,31 @@ Pre> In
 Post>In
 */
 
+// Utility!
+Array.prototype.top = function() {
+    return this[this.length - 1];
+};
+String.prototype.replaceAt = function(index, char) {
+    return this.substr(0, index) + char + this.substr(index + char.length);
+};
+
 function reverse_string(s) {
     for (var i = s.length - 1, r = ''; i >= 0; r += s[i--]) {}
     return r;
 }
-/**
+function swapPranthesis(arr) {
+    var swapped = arr;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == '(') {
+            swapped = swapped.replaceAt(i, ')');
+        } else if (arr[i] == ')') {
+            swapped = swapped.replaceAt(i, '(');
+        }
+    }
+    return swapped;
+}
+
+/**1
  * [prefixToInfix description]
  * @param  {string} exp         input string as prefix
  * @return {string} stack.top() in case of validity, infix expr
@@ -33,9 +53,9 @@ function prefixToInfix(exp) {
             return -1;
         }
     }
-    return stack.length == 1 ? stack[0] : -1;
+    return stack.length == 1 ? stack.top() : -1;
 }
-/**
+/**2
  * [postfixToInfix description]
  * @param  {string} exp          input string as postfix
  * @return {string} stack.top() in case of validity, infix expr
@@ -60,11 +80,159 @@ function postfixToInfix(exp) {
             return -1;
         }
     }
-    return stack.length == 1 ? stack[0] : -1;
+    return stack.length == 1 ? stack.top() : -1;
 }
+
+
+/**3
+ * [prefixToPostfix description]
+ * @param  {[type]} exp [description]
+ * @return {[type]}     [description]
+ */
+function prefixToPostfix(exp) {
+    exp = exp.split("");
+    rev_exp = reverse_string(exp);
+    stack = [];
+    for (var i = 0; i < rev_exp.length; i++) {
+        var c = rev_exp[i];
+        if (c.match("((?:[a-zA-Z])|(?:[0-9]+))")) {
+            stack.push(c);
+        } else if ("+-*/^".indexOf(c) != -1) {
+            if (stack.length >= 2) {
+                lhs = stack.pop();
+                rhs = stack.pop();
+                stack.push(lhs + rhs + c);
+            } else {
+                return -1;
+            }
+        } else {
+            return -1;
+        }
+    }
+    return stack.length == 1 ? stack.top() : -1;
+}
+/**4
+ * [postfixToPrefix description]
+ * @param  {[type]} exp [description]
+ * @return {[type]}     [description]
+ */
+function postfixToPrefix(exp) {
+    exp = exp.split("");
+    // rev_exp = reverse_string(exp);
+    stack = [];
+    for (var i = 0; i < exp.length; i++) {
+        var c = exp[i];
+        if (c.match("((?:[a-zA-Z])|(?:[0-9]+))")) {
+            stack.push(c);
+        } else if ("+-*/^".indexOf(c) != -1) {
+            if (stack.length >= 2) {
+                lhs = stack.pop();
+                rhs = stack.pop();
+                stack.push(c + rhs + lhs); //[c,rhs,lhs].join(" ")
+            } else {
+                return -1;
+            }
+        } else {
+            return -1;
+        }
+    }
+    return stack.length == 1 ? stack.top() : -1;
+}
+
+
+function infixToPrefix(exp) {
+  exp = swapPranthesis(exp);
+  exp = reverse_string(exp).split("");
+  prec = {
+      '^': 4,
+      '*': 3,
+      '/': 3,
+      '+': 2,
+      '-': 2,
+      '(': 1
+  };
+  opStack = [];
+  result = [];
+  result = [];
+  for (var i = 0; i < exp.length; i++) {
+      var c = exp[i];
+      if (c.match("((?:[a-zA-Z])|(?:[0-9]+))")) {
+          result.push(c);
+      } else if (c == '(') {
+          opStack.push(c);
+      } else if (c == ')') {
+          topTok = opStack.pop();
+          while (topTok != '(') {
+              topTok = opStack.pop();
+          }
+      } else if ("+-*/^".indexOf(c) != -1) {
+          while (opStack.length !== 0 && prec[opStack.top()] >= prec[c]) {
+              result.push(opStack.pop());
+          }
+          opStack.push(c);
+      } else {
+          throw "invalid character";
+      }
+  }
+  while (opStack.length !== 0) {
+      result.push(opStack.pop());
+  }
+  return reverse_string(result.join(""));
+}
+// console.log(infixToPrefix("("));
+
+
+
+function infixtoPostfix(exp) {
+    exp = exp.split("");
+    prec = {
+        '^': 4,
+        '*': 3,
+        '/': 3,
+        '+': 2,
+        '-': 2,
+        '(': 1
+    };
+    opStack = [];
+    result = [];
+    for (var i = 0; i < exp.length; i++) {
+        var c = exp[i];
+        if (c.match("((?:[a-zA-Z])|(?:[0-9]+))")) {
+            result.push(c);
+        } else if (c == '(') {
+            opStack.push(c);
+        } else if (c == ')') {
+            topTok = opStack.pop();
+            while (topTok != '(') {
+                result.push(topTok);
+                topTok = opStack.pop();
+            }
+        } else if ("+-*/^".indexOf(c) != -1) {
+            while (opStack.length !== 0 && prec[opStack.top()] >= prec[c]) {
+                result.push(opStack.pop());
+            }
+            opStack.push(c);
+        } else {
+            throw "invalid character";
+        }
+    }
+    while (opStack.length !== 0) {
+        result.push(opStack.pop());
+    }
+    return result.join("");
+}
+
+// console.log(infixtoPostfix("3 + 5"));
+// console.log(infixtoPostfix("( A + B ) * C - ( D - E ) * ( F + G )"));
+
 //TEST CASES:
 // prefixToInfix("+/356")
 // prefixToInfix("*+A-bc/-d3+-fgh")
 
-// postfixToInfix("12+3/")
-// postfixToInfix("512+4*+3-")
+// console.log(postfixToInfix("12+3/"));
+// console.log(postfixToInfix("512+4*+3-"));
+//
+//
+// prefixToPostfix("+35")
+// postfixToPrefix("135/*")
+//
